b05fe71da87c8e3c2aa0ace3cd7ce2fb90221e86
Alex Nudelman <alex.nudelman@clarityo.com>
2025-10-09 18:25:49 +0300

    Updates

--- src/utils/id.ts
+++
@@ -1,5 +0,0 @@
-const take = (value: string, length: number) => value.slice(0, length);
-
-export const createJoinCode = () => take(crypto.randomUUID().replace(/-/g, ''), 8);
-
-export const createShareSlug = () => take(crypto.randomUUID().replace(/-/g, ''), 12);
